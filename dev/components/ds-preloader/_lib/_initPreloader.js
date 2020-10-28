8ba6416334ab45be862a3c867dbcc9b6ae4486c0
Shkredov Dmitriy <shkredovdmitroy@gmail.com>
2020-10-29 00:21:22 +0300

    Add counter

--- dev/components/ds-preloader/_lib/_initPreloader.js
+++ dev/components/ds-preloader/_lib/_initPreloader.js
@@ -9,10 +9,55 @@ export default function initPreloader() {
 
   preloaderOpen();
 
-  window.onload = function() {
-    preloaderClose();
-    setTimeout(() => {
-      preloader.remove();
-    }, 2000);
-  };
+  let counter = 0;
+  const counterContainer = document.querySelector(".ds-preloader-counter");
+  const imgs = document.querySelectorAll("img");
+  const imgCount = imgs.length;
+
+  const perImg = Math.floor(100 / imgCount) + 1;
+
+  function updateCounter() {
+    counter += perImg;
+    if (counter > 0 && counter < 100) {
+      counterContainer.innerHTML = `${counter}%`;
+    } else {
+      counterContainer.innerHTML = `100%`;
+      preloaderClose();
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    }
+  }
+
+  function ifImageLoad() {
+    setTimeout(function() {
+      updateCounter();
+    }, 1500);
+  }
+
+  function ifImageError() {
+    setTimeout(function() {
+      updateCounter();
+    }, 1500);
+  }
+
+  function testImage(url) {
+    var tester = new Image();
+    tester.addEventListener("load", ifImageLoad);
+    tester.addEventListener("error", ifImageError);
+    tester.src = url;
+  }
+
+  imgs.forEach(img => {
+    testImage(img.src);
+  });
+
+  // window.onload = () => {
+  //   setTimeout(() => {
+  //     preloaderClose();
+  //     setTimeout(() => {
+  //       preloader.remove();
+  //     }, 2000);
+  //   }, 2000);
+  // };
 }
