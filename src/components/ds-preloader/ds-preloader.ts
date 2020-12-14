b51aecf74e88e735d5e112403f46dafde02bb156
Shkredov Dmitriy <ShkredovDmitriy@gmail.com>
2020-12-14 18:06:13 +0300

    Refactoring

--- src/components/ds-preloader/ds-preloader.ts
+++ src/components/ds-preloader/ds-preloader.ts
@@ -1,12 +1,14 @@
 import "./ds-preloader.scss";
 import create from "./_lib/_create";
 import open from "./_lib/_open";
+import load from "./_lib/_load";
 
 class DsPreloader {
 
   init() {
     create();
     open();
+    load();
   }
 }
 
